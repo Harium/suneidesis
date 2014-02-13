8c09948d1aee40510d3dc88cb5293f46c6041a1a
Yuri Pourre <yuriamayuri@gmail.com>
2014-02-12 23:43:29 -0200

    Better Action Parser

--- MedievalPerson/src/Terminal.java
+++ MedievalPerson/src/Terminal.java
@@ -17,21 +17,8 @@ public class Terminal {
 		RationalBeign thror = new RationalBeign("Thror", Gender.MALE);
 		
 		thrain.setBiologicalFather(thror);
-		
-		List<Action> actions = new ArrayList<Action>();
-		
-		Action killTroll = new Action();
-		
-		killTroll.setActor(thror); //Who did the action
-		killTroll.setWho(new Beign("Troll")); //Who suffered the action 
-		killTroll.setPlace(new Place("the top of the Silvertine")); //Where the action happens
-		killTroll.setAction("Cut"); //Action's name
-		killTroll.setWhereAction("throat"); //
-		killTroll.setWhen("10 years ago"); //When action happens
-		
-		actions.add(killTroll);
-		
-		thrain.setActions(actions);
+				
+		thrain.setActions(createActions(thror));
 		
 		ask("How are You?", thrain);
 		
@@ -47,11 +34,41 @@ public class Terminal {
 				
 	}
 	
+	private static List<Action> createActions(RationalBeign beign){
+
+		List<Action> actions = new ArrayList<Action>();
+		
+		Action killTroll = new Action();
+		
+		killTroll.setActor(beign); //Who did the action
+		killTroll.setWho(new Beign("a Troll")); //Who suffered the action 
+		killTroll.setPlace(new Place("at the top of Silvertine")); //Where the action happens
+		killTroll.setAction("Cut"); //Action's name
+		killTroll.setWhereAction("throat"); //
+		killTroll.setWhen("last week"); //When action happens
+		
+		actions.add(killTroll);
+		
+		Action createRing = new Action();
+		
+		createRing.setActor(new Beign("Sauron")); //Who did the action
+		createRing.setWho(new Beign("a ring")); //Who suffered the action 
+		createRing.setPlace(new Place("somewhere in Middle-Earth")); //Where the action happens
+		createRing.setAction("Create"); //Action's name
+		createRing.setWhereAction(""); //
+		createRing.setWhen("many years ago"); //When action happens
+		
+		actions.add(createRing);
+		
+		return actions;
+	}
+	
 	private static void ask(String query, RationalBeign beign){
 		
 		System.out.println(query.replace("?", ", "+beign.getName()+"?"));
 		
 		System.out.println(beign.listen(query));
+		
 	}
 	
 }
--- MedievalPerson/src/com/medievalperson/knowledge/SimpleActionParser.java
+++ MedievalPerson/src/com/medievalperson/knowledge/SimpleActionParser.java
@@ -10,39 +10,74 @@ public class SimpleActionParser implements ActionParser {
 
 	@Override
 	public String parse(String query, Beign beign) {
-		
-		String[] parts = query.replaceAll("\\?", "").split(" ");
-		
-		String queryAction = "";
-		
-		for(int i=0;i<parts.length;i++){
-			
-			if(parts[i].equalsIgnoreCase("about")){
-				
-				queryAction = parts[i+1];
-				break;
-				
-			}
-			
-		}
-		
-		List<Action> actions = beign.getActions();
 
-		for(Action action: actions){
+		String[] parts = query.replaceAll("\\?", "").split(" ");
+
+		String querySubject = "";
+
+		for(int i=0;i<parts.length;i++){
+
+			if(parts[i].equalsIgnoreCase("about")){
+
+				querySubject = parts[i+1];
+				break;
+
+			}
+
+		}
+
+		if(!querySubject.isEmpty()){
+
+			List<Action> actions = beign.getActions();
+
+			Action action = null;
 			
-			if(queryAction.equalsIgnoreCase(action.getAction())){
-				return "Yes. "+describeAction(action);
+			if(!Character.isUpperCase(querySubject.charAt(0))){
+				action = findActionByName(querySubject, actions);
+			}else{
+				action = findActionByActorsName(querySubject, actions);
+			}		
+
+			if(action!=null){
+				return describeAction(action);
 			}
 			
 		}
-		
+
 		return "No.";
 	}
-	
+
+	private Action findActionByActorsName(String actorName, List<Action> actions){
+
+		for(Action action: actions){
+
+			if(actorName.equalsIgnoreCase(action.getActor().getName())||actorName.equalsIgnoreCase(action.getWho().getName())){
+				return action;
+			}
+
+		}
+
+		return null;
+
+	}
+
+	private Action findActionByName(String actionName, List<Action> actions){
+
+		for(Action action: actions){
+
+			if(actionName.equalsIgnoreCase(action.getAction())){
+				return action;
+			}
+
+		}
+
+		return null;
+	}
+
 	public String describeAction(Action action){
-		
+
 		StringBuilder builder = new StringBuilder();
-		
+
 		builder.append("I heard that");
 		builder.append(" ");
 		builder.append(action.getActor().getName());
@@ -50,20 +85,18 @@ public class SimpleActionParser implements ActionParser {
 		builder.append(action.getAction().toLowerCase());
 		builder.append("s ");
 		builder.append(action.getWho().getName());
-		
+
 		if(!action.getWhereAction().isEmpty()){
 			builder.append("'s ");
-			builder.append(action.getWhereAction());
-			builder.append(" ");
+			builder.append(action.getWhereAction());			
 		}
-		
-		builder.append("at");
+
 		builder.append(" ");
 		builder.append(action.getPlace().getName());
 		builder.append(" ");
 		builder.append(action.getWhen());
-		
+
 		return builder.toString();
 	}
-	
+
 }
