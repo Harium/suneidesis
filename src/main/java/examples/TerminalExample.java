bda68b1e1f66cb7e229f5430882f737e56578213
Yuri Pourre <yuripourre@gmail.com>
2021-01-14 15:11:17 -0300

    Add example using terminal

---
+++ src/main/java/examples/TerminalExample.java
@@ -0,0 +1,26 @@
+package examples;
+
+import com.harium.suneidesis.chat.Parser;
+import com.harium.suneidesis.chat.box.BaseChatBox;
+import com.harium.suneidesis.chat.input.InputContext;
+import com.harium.suneidesis.chat.input.Terminal;
+import com.harium.suneidesis.chat.output.Output;
+
+public class TerminalExample {
+
+    public static void main(String[] args) {
+        BaseChatBox box = new BaseChatBox();
+        box.addParser(new Parser() {
+            @Override
+            public boolean parse(InputContext context, Output output) {
+                String sentence = context.getSentence();
+                output.print("Received: " + sentence);
+                return true;
+            }
+        });
+
+        Terminal terminal = new Terminal();
+        terminal.addBox(box);
+    }
+
+}
