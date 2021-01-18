0290b1171f42b8b91e8a170652a2b941b73d7a73
Yuri Pourre <yuripourre@gmail.com>
2021-01-18 16:42:11 -0300

    Add languages

--- src/main/java/com/harium/suneidesis/chat/input/InputContext.java
+++ src/main/java/com/harium/suneidesis/chat/input/InputContext.java
@@ -14,6 +14,8 @@ public class InputContext {
     public static final String CHANNEL_ID = "channel_id";
     public static final String CHANNEL_NAME = "channel_name";
 
+    public static final String LANGUAGE = "lang";
+
     private String sentence;
     private String originalSentence;
     private Parser currentParser;
