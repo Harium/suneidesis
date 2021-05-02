612c40c39fcb5668f9ffbd10c6fccb6364e20457
Yuri Pourre <yuripourre@gmail.com>
2021-05-02 13:25:04 -0300

    Exclude examples from jar

--- src/main/java/com/harium/suneidesis/chat/Parser.java
+++ src/main/java/com/harium/suneidesis/chat/Parser.java
@@ -5,6 +5,12 @@ import com.harium.suneidesis.chat.output.Output;
 
 public interface Parser {
 
+    /**
+     * Method that handles messages
+     * @param context - the context when receiving messages
+     * @param output - the output
+     * @return if the message was properly handled or another parser can try to handle it
+     */
     boolean parse(InputContext context, Output output);
 
 }
