b6fe9119c09f0d9649b3f918bd8e0a94832d08b2
Yuri Pourre <yuripourre@gmail.com>
2021-06-01 15:58:52 -0300

    Move attributes from Provenance to Action

--- src/main/java/com/harium/suneidesis/concept/Action.java
+++ src/main/java/com/harium/suneidesis/concept/Action.java
@@ -1,9 +1,86 @@
 package com.harium.suneidesis.concept;
 
+import com.harium.suneidesis.concept.measurement.Time;
+
 public class Action extends Concept {
+
+    public static final String ATTRIBUTE_SUBJECT = "subject";
+    public static final String ATTRIBUTE_PREDICATE = "predicate";
+    public static final String ATTRIBUTE_OBJECT = "object";
+    public static final String ATTRIBUTE_OBJECT_PART = "object_part";
+    public static final String ATTRIBUTE_PLACE = "place";
+    public static final String ATTRIBUTE_TIME = "time";
+    public static final String ATTRIBUTE_PHENOMENON = "phenomenon";
 
     public Action(String name) {
         super(name, ConceptType.ACTION);
+    }
+
+    public Concept getSubject() {
+        return getAttributes().get(ATTRIBUTE_SUBJECT);
+    }
+
+    public Action subject(Concept subject) {
+        getAttributes().insert(ATTRIBUTE_SUBJECT, subject);
+        return this;
+    }
+
+    public Concept getPredicate() {
+        return getAttributes().get(ATTRIBUTE_PREDICATE);
+    }
+
+    public Action predicate(Concept predicate) {
+        getAttributes().insert(ATTRIBUTE_PREDICATE, predicate);
+        return this;
+    }
+
+    public Concept getObject() {
+        return getAttributes().get(ATTRIBUTE_OBJECT);
+    }
+
+    public Action object(Concept object) {
+        getAttributes().insert(ATTRIBUTE_OBJECT, object);
+        return this;
+    }
+
+    public Concept getObjectPart() {
+        return getAttributes().get(ATTRIBUTE_OBJECT_PART);
+    }
+
+    public Action objectPart(Concept objectPart) {
+        getAttributes().insert(ATTRIBUTE_OBJECT_PART, objectPart);
+        return this;
+    }
+
+    public Concept getPlace() {
+        return getAttributes().get(ATTRIBUTE_PLACE);
+    }
+
+    public Action place(Place place) {
+        getAttributes().insert(ATTRIBUTE_PLACE, place);
+        return this;
+    }
+
+    public Concept getTime() {
+        return getAttributes().get(ATTRIBUTE_TIME);
+    }
+
+    public Action time(Time time) {
+        getAttributes().insert(ATTRIBUTE_TIME, time);
+        return this;
+    }
+
+    public Concept getWeather() {
+        return getAttributes().get(ATTRIBUTE_PHENOMENON);
+    }
+
+    public Action weather(Phenomenon phenomenon) {
+        return phenomenon(phenomenon);
+    }
+
+    public Action phenomenon(Phenomenon phenomenon) {
+        getAttributes().insert(ATTRIBUTE_PHENOMENON, phenomenon);
+        return this;
     }
 
     @Override
--- src/main/java/com/harium/suneidesis/concept/helper/Provenance.java
+++ src/main/java/com/harium/suneidesis/concept/helper/Provenance.java
@@ -18,14 +18,6 @@ public class Provenance {
     public static final String ATTRIBUTE_ACQUISITION_METHOD = "acquisition_method";
     public static final String ATTRIBUTE_ACQUISITION_MEDIUM = "acquisition_medium";
 
-    public static final String ATTRIBUTE_SUBJECT = "subject";
-    public static final String ATTRIBUTE_PREDICATE = "predicate";
-    public static final String ATTRIBUTE_OBJECT = "object";
-    public static final String ATTRIBUTE_OBJECT_PART = "object_part";
-    public static final String ATTRIBUTE_PLACE = "place";
-    public static final String ATTRIBUTE_TIME = "time";
-    public static final String ATTRIBUTE_PHENOMENON = "phenomenon";
-
     private final Concept concept;
 
     public Provenance(Concept concept) {
@@ -56,101 +48,6 @@ public class Provenance {
 
     public static void setSourceId(Concept concept, Concept sourceId) {
         concept.getAttributes().insert(ATTRIBUTE_SOURCE_ID, sourceId);
-    }
-
-    public Concept getSubject() {
-        return concept.getAttributes().get(ATTRIBUTE_SUBJECT);
-    }
-
-    public Provenance subject(Concept subject) {
-        setSubject(concept, subject);
-        return this;
-    }
-
-    public static void setSubject(Concept concept, Concept subject) {
-        concept.getAttributes().insert(ATTRIBUTE_SUBJECT, subject);
-    }
-
-    public Concept getPredicate() {
-        return concept.getAttributes().get(ATTRIBUTE_PREDICATE);
-    }
-
-    public Provenance predicate(Concept predicate) {
-        setPredicate(concept, predicate);
-        return this;
-    }
-
-    public static void setPredicate(Concept concept, Concept predicate) {
-        concept.getAttributes().insert(ATTRIBUTE_PREDICATE, predicate);
-    }
-
-    public Concept getObject() {
-        return concept.getAttributes().get(ATTRIBUTE_OBJECT);
-    }
-
-    public Provenance object(Concept object) {
-        setObject(concept, object);
-        return this;
-    }
-
-    public static void setObject(Concept concept, Concept object) {
-        concept.getAttributes().insert(ATTRIBUTE_OBJECT, object);
-    }
-
-    public Concept getObjectPart() {
-        return concept.getAttributes().get(ATTRIBUTE_OBJECT_PART);
-    }
-
-    public Provenance objectPart(Concept objectPart) {
-        setObjectPart(concept, objectPart);
-        return this;
-    }
-
-    public static void setObjectPart(Concept concept, Concept objectPart) {
-        concept.getAttributes().insert(ATTRIBUTE_OBJECT_PART, objectPart);
-    }
-
-    public Concept getPlace() {
-        return concept.getAttributes().get(ATTRIBUTE_PLACE);
-    }
-
-    public Provenance place(Place place) {
-        setPlace(concept, place);
-        return this;
-    }
-
-    public static void setPlace(Concept concept, Concept place) {
-        concept.getAttributes().insert(ATTRIBUTE_PLACE, place);
-    }
-
-    public Concept getTime() {
-        return concept.getAttributes().get(ATTRIBUTE_TIME);
-    }
-
-    public Provenance time(Time time) {
-        setTime(concept, time);
-        return this;
-    }
-
-    public static void setTime(Concept concept, Concept time) {
-        concept.getAttributes().insert(ATTRIBUTE_TIME, time);
-    }
-
-    public Concept getWeather() {
-        return concept.getAttributes().get(ATTRIBUTE_PHENOMENON);
-    }
-
-    public Provenance weather(Phenomenon phenomenon) {
-        return phenomenon(phenomenon);
-    }
-
-    public Provenance phenomenon(Phenomenon phenomenon) {
-        setPhenomenon(concept, phenomenon);
-        return this;
-    }
-
-    public static void setPhenomenon(Concept concept, Concept phenomenon) {
-        concept.getAttributes().insert(ATTRIBUTE_PHENOMENON, phenomenon);
     }
 
     public Concept getAcquisitionMethod() {
