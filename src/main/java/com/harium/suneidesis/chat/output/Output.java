adc07a49013b37f6c4f6d18410bc56f85803992d
Yuri Pourre <yuripourre@gmail.com>
2021-08-10 14:54:02 -0300

    Add method to produce file from byteArray

--- src/main/java/com/harium/suneidesis/chat/output/Output.java
+++ src/main/java/com/harium/suneidesis/chat/output/Output.java
@@ -1,5 +1,9 @@
 package com.harium.suneidesis.chat.output;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public interface Output {
     default void print(String sentence) {
         print(sentence, null);
@@ -7,5 +11,10 @@ public interface Output {
 
     void print(String sentence, OutputContext context);
 
-    void produceFile(String path, String description);
+    default void produceFile(String path, String description) throws IOException {
+        byte[] content = Files.readAllBytes(Paths.get(path));
+        produceFile(content, description);
+    }
+
+    void produceFile(byte[] data, String description);
 }
