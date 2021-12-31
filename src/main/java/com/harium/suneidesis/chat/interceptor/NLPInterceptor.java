a15ee8a1e79c018a557ffd886a850a93e9f05036
Yuri Pourre <yuripourre@gmail.com>
2021-12-31 13:47:06 -0800

    Fix NLPInterceptor

--- src/main/java/com/harium/suneidesis/chat/interceptor/NLPInterceptor.java
+++ src/main/java/com/harium/suneidesis/chat/interceptor/NLPInterceptor.java
@@ -7,29 +7,61 @@ import com.harium.suneidesis.chat.output.Output;
 import com.harium.suneidesis.linguistic.nlp.NLP;
 import com.harium.suneidesis.linguistic.nlp.pos.TagPair;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class NLPInterceptor implements Interceptor {
 
-    protected NLP nlp;
+    public static final String PROPERTY_NLP = "NLP";
+    private static final String DEFAULT_LANGUAGE = "DEFAULT_LANG";
+
+    protected Map<String, NLP> nlpMap = new HashMap<>();
 
     public NLPInterceptor(NLP nlp) {
-        this.nlp = nlp;
+        this(DEFAULT_LANGUAGE, nlp);
+    }
+
+    public NLPInterceptor(String language, NLP nlp) {
+        if (!DEFAULT_LANGUAGE.equals(language)) {
+            nlpMap.put(DEFAULT_LANGUAGE, nlp);
+        }
+        if (language != null) {
+            nlpMap.put(language, nlp);
+        }
     }
 
     @Override
     public void preParsing(InputContext input, Output output) {
         String sentence = input.getOriginalSentence();
+        String language = input.getLanguage();
 
-        // POSTag
+        NLP nlp = getNLP(language);
+        if (nlp == null) {
+            return;
+        }
+
+        // Process Part Of Speech (POS) tags
         TagPair[] tags = nlp.posTag(sentence);
         // Replace words by lemma
         for (TagPair tagPair : tags) {
             String lemma = nlp.lemmatize(tagPair.getWord());
             tagPair.setWord(lemma);
         }
+
+        input.getProperties().put(PROPERTY_NLP, tags);
+    }
+
+    private NLP getNLP(String language) {
+        NLP nlp = nlpMap.get(language);
+        if (nlp == null) {
+            return nlpMap.get(DEFAULT_LANGUAGE);
+        }
+        return nlp;
     }
 
     @Override
     public void postParsing(InputContext input, Output output, Parser parser) {
 
     }
+
 }
