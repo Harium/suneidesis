743061fee06163d3f713d6b2a81d2ee81aa23771
Yuri Pourre <yuripourre@gmail.com>
2022-07-30 12:18:20 -0700

    Update LengthConverter

--- src/main/java/com/harium/suneidesis/math/converter/LengthConverter.java
+++ src/main/java/com/harium/suneidesis/math/converter/LengthConverter.java
@@ -9,20 +9,59 @@ public class LengthConverter implements MeasurementConverter {
     public static final String METER = "meter";
     public static final String MILE = "mile";
     public static final String KILOMETER = "kilometer";
+    public static final String FOOT = "foot";
+    public static final String INCH = "inch";
 
     @Override
     public String convert(String amount, String unit, String outputUnit) {
-        BigDecimal value = new BigDecimal(amount);
         if (METER.equals(unit) && KILOMETER.equals(outputUnit)) {
-            return value.divide(new BigDecimal(1000)).toPlainString();
+            return meterToKilometer(amount);
         } else if (KILOMETER.equals(unit) && METER.equals(outputUnit)) {
-            return value.multiply(new BigDecimal(1000)).toPlainString();
-        } else if (KILOMETER.equals(unit) && MILE.equals(outputUnit)) {
-            return value.multiply(new BigDecimal(0.62137119)).toPlainString();
+            return kilometerToMeter(amount);
         } else if (MILE.equals(unit) && KILOMETER.equals(outputUnit)) {
-            return value.multiply(new BigDecimal(1.609344)).toPlainString();
+            return mileToKilometer(amount);
+        } else if (KILOMETER.equals(unit) && MILE.equals(outputUnit)) {
+            return kilometerToMile(amount);
+        } else if (METER.equals(unit) && MILE.equals(outputUnit)) {
+            // Convert to common unit
+            String kilometer = meterToKilometer(amount);
+            return kilometerToMile(kilometer);
+        } else if (METER.equals(unit) && FOOT.equals(outputUnit)) {
+            return meterToFoot(amount);
+        } else if (METER.equals(unit) && INCH.equals(outputUnit)) {
+            return meterToInch(amount);
         }
 
         return "";
+    }
+
+    private String meterToKilometer(String amount) {
+        BigDecimal value = new BigDecimal(amount);
+        return value.divide(BigDecimal.valueOf(1000)).toPlainString();
+    }
+
+    private String meterToFoot(String amount) {
+        BigDecimal value = new BigDecimal(amount);
+        return value.multiply(BigDecimal.valueOf(3.28084)).toPlainString();
+    }
+
+    private String meterToInch(String amount) {
+        BigDecimal value = new BigDecimal(amount);
+        return value.multiply(BigDecimal.valueOf(39.3701)).toPlainString();
+    }
+
+    private String kilometerToMeter(String amount) {
+        BigDecimal value = new BigDecimal(amount);
+        return value.multiply(BigDecimal.valueOf(1000)).toPlainString();
+    }
+
+    private String kilometerToMile(String amount) {
+        BigDecimal value = new BigDecimal(amount);
+        return value.multiply(BigDecimal.valueOf(0.62137119)).toPlainString();
+    }
+
+    private String mileToKilometer(String amount) {
+        BigDecimal value = new BigDecimal(amount);
+        return value.multiply(BigDecimal.valueOf(1.609344)).toPlainString();
     }
 }
